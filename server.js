4e3eaec28d6f6ccd9f985fcc60e6435b4a0336c2
mozart <mozart@watchmile.com>
2021-03-29 18:29:38 +0900

    add tutorial route path for app listen in server.js

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const cors = require("cors");
 
 const app = express();
 
-const db = require("./app/models");
-db.sequelize.sync();
-
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-});
-
 var corsOptions = {
     origin: "http://localhost:8081"
 };
@@ -21,9 +14,15 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+
+db.sequelize.sync();
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to lee application." });
+    res.json({ message: "Welcome to bezkoder application." });
 });
+
+require("./app/routes/tutorial.routes")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
