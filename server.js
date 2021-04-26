60154557db3d0b8e30ee11646960bc8f9c03bf00
mozart <mozart@watchmile.com>
2021-04-26 22:03:21 +0900

    create pooling for mysql interrupted

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require('./app/routes/user.routes')(app);
 
 // require("./app/routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
