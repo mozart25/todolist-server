8d15eb2ea579ed7b1354689b8987d7b8d9e09a98
mozart <mozart@watchmile.com>
2021-04-27 00:56:24 +0900

    cors changed

--- server.js
+++ server.js
@@ -19,8 +19,14 @@ var corsOptions = {
 };
 
 // app.use(cors(corsOptions));
-app.use(cors());
-
+// app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(bodyParser.json());
 
