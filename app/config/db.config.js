db185f2042095a083065ffd04b677e532addc51e
mozart <mozart@watchmile.com>
2021-04-27 19:04:20 +0900

    change cors adjustment 3rd

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -12,3 +12,18 @@ module.exports = {
         idle: 10000
     }
 };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "qwe123,.",
+//     DB: "todolist",
+//     dialect: "mysql",
+//     timezone: '+09:00',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
--- server.js
+++ server.js
@@ -8,26 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-require("./app/routes/tutorial.routes")(app);
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
-
-
 global.__basedir = __dirname;
-
-var corsOptions = {
-    origin: "http://localhost:3001"
-};
-
-// app.use(cors(corsOptions));
-
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
 
 app.use(bodyParser.json());
 
@@ -47,9 +28,10 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to lee's application." });
 });
 
+require("./app/routes/tutorial.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
-
-// require("./app/routes")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
